a241d55eed75445e609a30687fa40ddcd8a5c261
Justicesibisi <djmangsa@gmail.com>
2024-10-30 14:48:08 +0200

    Test checker

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,20 +1,26 @@
-#!/usr/bin/python3
-""" BasicCache module
-"""
+#!/usr/bin/env python3
+
+'''Task 0: Basic dictionary
+'''
+
+
 from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """ BasicCache class that inherits from BaseCaching
-        This cache has no limit on the number of items
-    """
+    '''A class `BasicCache` that inherits from `BaseCaching`
+       and is a caching system
+    '''
 
     def put(self, key, item):
-        """ Add an item in the cache """
-        if key is None or item is None:
-            return
-        self.cache_data[key] = item
+        '''assign to the dictionary `self.cache_data` the
+           `item` value for the key `key`
+        '''
+        if key is not None and item is not None:
+            self.cache_data[key] = item
 
     def get(self, key):
-        """ Get an item by key """
+        '''return the value in `self.cache_data` linked to `key`
+        '''
+
         return self.cache_data.get(key, None)
